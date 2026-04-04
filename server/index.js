cb2aef2608cd57541afb762f320f708862a30569
user.name: "AbdullahKhamoor <abdullah.khamoor4@gmail.com>
2026-04-04 20:40:04 +0500

    index update

--- server/index.js
+++ server/index.js
@@ -24,14 +24,14 @@ app.get("/", (req,res) => {
 })
 
 
-app.post('/create', (req,res) => {
+app.post('/api/create', (req,res) => {
     UserModel.create(req.body)
     .then(user => res.json(user))
     .catch(err => res.json(err))
 })
 
 
-app.put('/update/:id', (req,res)=>{
+app.put('/api/update/:id', (req,res)=>{
     const id = req.params.id
     UserModel.findByIdAndUpdate({_id: id}, {
         name: req.body.name,
@@ -42,7 +42,7 @@ app.put('/update/:id', (req,res)=>{
     .catch(err => res.json(err))
 } )
 
-app.delete('/deleteuser/:id',(req,res)=> {
+app.delete('/api/deleteuser/:id',(req,res)=> {
     const id = req.params.id;
     UserModel.findByIdAndDelete({_id:id})
     .then(response => res.json(response))
@@ -50,9 +50,9 @@ app.delete('/deleteuser/:id',(req,res)=> {
 })
 
 
-// app.listen(3001, ()=>{
-//    console.log("Server is Running")
-// })
+app.listen(3001, ()=>{
+   console.log("Server is Running")
+})  
 
 // export default function handler(req, res) {
 //   res.json({ message: "API working" });
